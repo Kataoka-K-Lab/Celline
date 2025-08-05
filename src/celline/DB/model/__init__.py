a55d3f4d605a9cb1fc30893791ee5934bc618b87
YUYA556223 <yuya.sato.jp@gmail.com>
2025-08-05 15:34:28 +0900

    Added public DB types

--- src/celline/DB/model/__init__.py
+++ src/celline/DB/model/__init__.py
@@ -6,4 +6,8 @@ from celline.DB.model.cncb_prjca import CNCB_PRJCA
 from celline.DB.model.cncb_cra import CNCB_CRA
 from celline.DB.model.cncb_crr import CNCB_CRR
 
+from celline.DB.model.ae_study import AE_Study
+from celline.DB.model.ae_sample import AE_Sample
+from celline.DB.model.ae_run import AE_Run
+
 from celline.DB.model.transcriptome import Transcriptome
